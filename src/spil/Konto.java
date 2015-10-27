c2162c26746a9b054915a9dc1346c6e55f035bd1
Martin Dalby <Martin Dalby@10.16.128.146>
2015-10-27 15:20:40 +0100

    Tilføjet metoder til konto Klassen. - Kristian, Alexander, Sara, Martin

--- src/spil/Konto.java
+++ src/spil/Konto.java
@@ -4,11 +4,22 @@ public class Konto {
 
 	private int beholdning;
 	
+	public Konto(){
+		beholdning = 1000;
+	}
 	public void setBeholdning(int a){
 		beholdning += a;
+		if(beholdning < 0)
+			beholdning = 0;
 	}
 	
 	public int getBeholdning(){
 		return beholdning;
+	}
+	
+	
+	
+	public String toString(){
+		return(" har " + beholdning + " kroner");
 	}
 }
