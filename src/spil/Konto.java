9cc0c89828ff2d957b2c4d986b422178125ba210
Martin Dalby <Martin Dalby@10.16.128.146>
2015-10-27 15:11:52 +0100

    Tilføjet Konto klassen

---
+++ src/spil/Konto.java
@@ -0,0 +1,14 @@
+package spil;
+
+public class Konto {
+
+	private int beholdning;
+	
+	public void setBeholdning(int a){
+		beholdning += a;
+	}
+	
+	public int getBeholdning(){
+		return beholdning;
+	}
+}
