4684b572509f0fe814adc119fb6b4179434acfff
Martin Dalby <Martin Dalby@10.16.143.101>
2015-10-29 14:42:37 +0100

    Tilføjet Felt klassen

---
+++ src/Felt.java
@@ -0,0 +1,16 @@
+
+public class Felt {
+
+	private int[] feltVærdi = {250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650};
+	private String[] feltTekst = {};
+	
+	public int getFeltVærdi(int a){
+		return feltVærdi[a];
+	}
+	
+	public String getFeltTekst(int a){
+		return feltTekst[a];
+	}
+	
+	
+}
